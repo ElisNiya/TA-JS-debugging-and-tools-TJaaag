156b14b2839caf4bb140b5efc5a2902e581c5f5f
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-11-08 14:18:51 +0100

    Update script.js

---
+++ block-BJaafo/code/script.js
@@ -0,0 +1,20 @@
+Write two tests for the following functions
+Get full name
+
+let fullName(first, last){
+  first + " " + second
+}
+Write a function that takes two input firstName and lastName and returns full name. You will get the full name by adding first and last name with an space.
+After writing the function write two tests for above function
+if (first === "" || last == "" ){
+
+    throw new Error("Name should be not empty")
+    )
+}
+Make the first test fail and look at the output
+After making the first test fail do you see the output of the second test?
+Calculate total amount
+Write a function that takes two input amount and taxRate and returns the total amount by amount + (amount * taxRate)
+Write two tests for the above function
+Make the first test fail and look at the output
+After making the first test fail do you see the output of the second test?
